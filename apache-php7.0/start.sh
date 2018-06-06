d802dd8fe238f0270a24c1521eed198300dd420a
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2018-06-06 22:06:38 +0200

    Update robo, add php cli debug alias, fix hosts file

--- apache-php7.0/start.sh
+++ apache-php7.0/start.sh
@@ -2,7 +2,7 @@
 
 # Handle the IP change
 cat /etc/hosts | grep -v "inchoo.host.internal" > /etc/hosts
-echo -e "`/sbin/ip route|awk '/default/ { print $3 }' | grep -v ppp` inchoo.host.internal" | tee -a /etc/hosts > /dev/null
+echo "`/sbin/ip route|awk '/default/ { print $3 }' | grep -v ppp` inchoo.host.internal" | tee -a /etc/hosts > /dev/null
 
 # Run services
 nohup /usr/sbin/php-fpm7.0 &
