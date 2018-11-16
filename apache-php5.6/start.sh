e44f7749dafe2b7516c259387a89a729a3812b26
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2018-11-16 23:45:52 +0100

    Add default apache config

--- apache-php5.6/start.sh
+++ apache-php5.6/start.sh
@@ -4,6 +4,9 @@
 cat /etc/hosts | grep -v "inchoo.host.internal" > /etc/hosts
 echo "`/sbin/ip route|awk '/default/ { print $3 }' | grep -v ppp` inchoo.host.internal" | tee -a /etc/hosts > /dev/null
 
+# Setup Apache document root
+sed "s#DOCUMENT_ROOT#${DOCUMENT_ROOT:-/var/www/html}#g" -i /etc/apache2/sites-available/000-default.conf
+
 # Run services
 nohup /usr/sbin/php-fpm5.6 &
 /usr/sbin/apache2ctl -D FOREGROUND
