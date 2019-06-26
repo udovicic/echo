0620bee60383248bcce6d06a00cb7845469007b3
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2019-06-26 08:47:15 +0200

    Bump new versions of helper scripts

---
+++ update_helpers.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+UPDATE_SCRIPTS="php composer.phar selfupdate && php n98-magerun2.phar selfupdate && php n98-magerun.phar selfupdate && robo.phar self:update"
+
+# General purpose images
+for D in apache-php*; do
+	if [ -d "${D}" ]; then
+		cd ${D}/scripts
+		bash -c "$UPDATE_SCRIPTS"
+		cd ../..
+	fi
+done
+
+# This one is a special case
+cd apache-sylius/scripts/
+php composer.phar selfupdate && robo.phar self:update
+cd ../..
