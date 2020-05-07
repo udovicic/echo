69375e37a92118d6bd3039d6fc1488f6cd70a7bf
Stjepan Udovicic <stjepan.udovicic@inchoo.net>
2020-05-07 11:54:48 +0200

    Downgrade n98 to 3.2.0, to bypass ram usage issue

--- update_helpers.sh
+++ update_helpers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UPDATE_SCRIPTS="php composer.phar selfupdate && php n98-magerun2.phar selfupdate && php n98-magerun.phar selfupdate && php robo.phar self:update && php deployer.phar self-update"
+UPDATE_SCRIPTS="php composer.phar selfupdate && php n98-magerun.phar selfupdate && php robo.phar self:update && php deployer.phar self-update"
 
 # General purpose images
 for D in apache-php*; do
